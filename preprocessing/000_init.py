0682d53b85a779db76afe2810cb9fa7098717b43
mhassantrq <mhassantrq@gmail.com>
2025-09-24 18:22:20 +0500

    added preprocessing 000

---
+++ preprocessing/000_init.py
@@ -0,0 +1,7 @@
+msg = """
+        It is stated that many libraries used in the following preprocessing
+        files, are separately explained in the libraries folder in root folder.
+        It may help to go through them beforehand.
+"""
+
+print(msg)
