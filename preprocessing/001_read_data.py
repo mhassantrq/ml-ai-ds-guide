cba92a9697bd0ffef6da35cae3c778ebf40a2f64
mhassantrq <mhassantrq@gmail.com>
2025-09-30 20:29:58 +0500

    preprocessing - added read data using csv module

--- preprocessing/001_read_data.py
+++ preprocessing/001_read_data.py
@@ -1,9 +1,8 @@
 """
-Dataset can be loaded in various ways. Lets explore them below one by one:
+Dataset can be loaded in various ways. Lets explore them.
 """
 
 # 1. Using pandas
-
 
 import pandas as pd
 
@@ -12,3 +11,15 @@ df = pd.read_csv('data/data.csv')
 
 #   print the data frame to view the file data
 print(df)
+
+#   for various other input files using pandas see pandas file in libraries and modules directory
+
+
+# 2. Using csv
+
+import csv
+
+with open('data/data.csv', newline='') as f:
+    r = csv.reader(f)
+    for row in r:
+        print(row)
