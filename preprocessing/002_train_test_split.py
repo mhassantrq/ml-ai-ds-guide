1f722a4361d2493195ff543ccb25f8e5218ec76b
Hassan <mhassantrq@gmail.com>
2025-10-01 21:38:02 +0500

    Update 002_train_test_split.py

--- preprocessing/002_train_test_split.py
+++ preprocessing/002_train_test_split.py
@@ -4,14 +4,23 @@ Training data is used to train the model while testing data is used to view how 
 has been achieved by the trained model.
 """
 
+# import pandas to read cav file in data frame
 import pandas as pd
+
+# import train test split from sklearn to split the data
 from sklearn.model_selection import train_test_split
 
+# read file
 df = pd.read_csv('data/data.csv')
 
+# x values include city and age from data frame
 X = df[['city', 'age']]
+
+# y variable salary, to be predicted based on coty and age
 y = df['salary']
 
+# split
+# test size=0.3 means that data to be split 70 and 30 percent for train and test respectively
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 print(f'records of training set {len(X_train)}')
