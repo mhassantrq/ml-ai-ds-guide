b59fa72fb662c4c7ab20858d2ecee3190881f47e
Hassan <mhassantrq@gmail.com>
2025-10-01 21:35:27 +0500

    Update 000_linear_regression_using_scipy.py

--- regression/linear/000_linear_regression_using_scipy.py
+++ regression/linear/000_linear_regression_using_scipy.py
@@ -2,13 +2,17 @@
 Linear Regression using scipy
 """
 
+# import stats from scipy package
 from scipy import stats
+# import matplotlib to plot graph of data
 import matplotlib.pyplot as plt
 import random
 
+# generating random values for x and y labels
 x_values = [random.randint(10, 90) for _ in range(15)]
 y_values = [random.randint(80, 250) for _ in range(15)]
 
+# put values of x and y on graph
 plt.scatter(x_values, y_values)
 #   plt.show()
 
