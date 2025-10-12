729b9e7e0ee0411f0103289ff8de3e6d561600e9
mhassantrq <mhassantrq@gmail.com>
2025-10-12 18:28:12 +0500

    text selection and split tokenization in 001 zero

--- from zero/001.py
+++ from zero/001.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 df = pd.read_csv('data/dataset.csv')
 
@@ -9,3 +10,22 @@ for r in text_list:
     original_text += r
 
 print(f'First 1000 characters in original text \n {original_text[:1000]}')
+
+#   lets see how many characters does it have
+print(len(original_text))   #   outputs 65 million plus.
+
+#   we ll only use 10000 characters for further understanding
+updated_text = original_text[:10000]
+print(len(updated_text))
+
+
+#   lets tokenize them
+tokens = updated_text.split(' ')
+print(tokens[:10])
+
+"""
+this above given straight forward method of tokenization is not useful as white spaces are removed and we cannot use tokens' sequence
+
+lets use regular expressions for this purpose
+"""
+
