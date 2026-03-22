5cb5c690705fb58059f6e2b185e5c261bcce2f04
mhassantrq <mhassantrq@gmail.com>
2026-03-22 19:37:54 +0500

    intermediate pending

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-*under construction*
+*under construction*,
 *for educational purpose only*
 
 *All about Machine Learning, Artificial Intelligence and Data Science*
---
+++ preprocessing/004_regex.py
@@ -0,0 +1,11 @@
+"""
+pending
+
+Regular expressions are easy way to extract information from given text
+
+
+"""
+
+import re
+
+text = "this is a sentence to check for regular expressions"
