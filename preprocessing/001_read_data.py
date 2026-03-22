4323609f67f419c524e57928fefc38a71cf1f709
mhassantrq <mhassantrq@gmail.com>
2026-03-22 21:11:22 +0500

    updated preprocessing-read data

--- preprocessing/001_read_data.py
+++ preprocessing/001_read_data.py
@@ -1,5 +1,5 @@
 """
-Dataset can be loaded in various ways. Lets explore them.
+Dataset can be loaded in various ways. Lets explore them one by one.
 """
 
 # 1. Using pandas
@@ -12,10 +12,19 @@ df = pd.read_csv('data/data.csv')
 #   print the data frame to view the file data
 print(df)
 
-#   for various other input files using pandas see pandas file in libraries and modules directory
+"""
+pandas can be used to read many types of files such as:
+    read_csv:       to read csv file
+    read_excel:     to read excel files
+    read_json:      to read json files
+    read_sql:       to read sql files
+    read_spss:      to read a spss file
+
+    and more.
+"""
 
 
-# 2. Using csv
+# 2. Using csv library
 
 import csv
 
@@ -23,3 +32,10 @@ with open('data/data.csv', newline='') as f:
     r = csv.reader(f)
     for row in r:
         print(row)
+
+
+# 3. using python own read files
+
+with open('data/data.csv') as f:
+    file = f.read()
+print(file)
