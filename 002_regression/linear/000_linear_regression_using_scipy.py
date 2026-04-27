a578fe6c21f970cbb7ce51d0aa03d6217761f28e
mhassantrq <mhassantrq@gmail.com>
2026-04-27 19:26:14 +0500

    lin reg using scipy, added explanation

--- 002_regression/linear/000_linear_regression_using_scipy.py
+++ 002_regression/linear/000_linear_regression_using_scipy.py
@@ -18,8 +18,16 @@ plt.scatter(x_values, y_values)
 
 m, y, r, p, err = stats.linregress(x_values, y_values)
 
+#   m:      slope. it shows the change in x and y relation. positive means upward trend, while negative shows downward trend.
+#   y:      y-intercept.
+#   r:      correlation coefficient. values between 1 and -1. near to 1 or -1 means strong relationship.
+#   p:      p-value. if less than 0.05. it means relation is significant. above 0.05, then due to randomness.
+#   err:    standard error.
+
 lin_model = [m * var + y for var in x_values]
 
 plt.plot(x_values, lin_model)
 
-plt.show()
+#plt.show()
+
+print(r)
