11f01b0ada30639b93de5ea971a20fbfd1d778b3
mhassantrq <mhassantrq@gmail.com>
2026-04-27 18:10:58 +0500

    added linear regression using sklearn

---
+++ regression/linear/001_linear_regression_using_sklearn.py
@@ -0,0 +1,22 @@
+from sklearn import linear_model
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+
+x_data = [5,6,2,10,15,20,7,8]
+y_data = [50,55,25,110,150,180,80,90]
+
+plt.scatter(x_data,y_data)
+
+plt.xlabel = 'Area'
+plt.xlabel = 'Price'
+
+x_data = np.array(x_data).reshape(-1,1)
+
+lin_model = linear_model.LinearRegression()
+lin_model.fit(x_data, y_data)
+
+print(lin_model.predict([[50]]))
+
+plt.show()
