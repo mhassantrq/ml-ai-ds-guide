ff025d5c334c20811594cb7e205d03239d19cf2b
mhassantrq <mhassantrq@gmail.com>
2026-05-03 20:38:06 +0500

    basic level topic modelling using lda, preprocessing pending...

--- topic_modelling/001_lda_using_sklearn.py
+++ topic_modelling/001_lda_using_sklearn.py
@@ -1,6 +1,21 @@
 import pandas as pd
-
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
 
 df = pd.read_csv('data/dataset.csv')
 
-print(df['text'][0:5])
+vect = CountVectorizer()
+words_count = vect.fit_transform(df['text'][0:5])
+
+words = vect.get_feature_names_out()
+
+lda = LatentDirichletAllocation(n_components=2, random_state=43)
+lda.fit(words_count)
+
+t_list = []
+
+for t in lda.components_:
+    top = t.argmax()
+    t_list.append(words[top])
+
+print(t_list)
