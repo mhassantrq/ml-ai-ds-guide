e72e5e056d72c95ce6dc28e605415de3146cd186
mhassantrq <mhassantrq@gmail.com>
2026-05-05 17:18:16 +0500

    added decision tree visualization

--- 007_classification/000_decision_tree_sklearn.py
+++ 007_classification/000_decision_tree_sklearn.py
@@ -3,15 +3,24 @@ Decision Tree using sklearn
 """
 
 import pandas as pd
-from sklearn import tree
-
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('data/classification01.csv')
 
 #print(df)
 
-dt_model = tree.DecisionTreeClassifier()
+dt_model = DecisionTreeClassifier()
 
 dt_model.fit(df[['study', 'attendance', 'assignments']], df.result)
 
 print(dt_model.predict([[1,70,2]]))
+
+
+"""
+Decision Plot Tree
+
+plt.figure(figsize=(10,6))
+plot_tree(dt_model, feature_names=df[['study', 'attendance', 'assignments']].columns, filled=True)
+plt.show()
+"""
