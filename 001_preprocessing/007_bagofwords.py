d685089c139b27a419b4763a007c592c990ca1a7
mhassantrq <mhassantrq@gmail.com>
2026-05-09 13:53:50 +0500

    added explanation to bagofwords and ngrams file

--- 001_preprocessing/007_bagofwords.py
+++ 001_preprocessing/007_bagofwords.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from collections import defaultdict
 
+"""
+Reading dataset, but only storing first 1000 rows in the text variable for preprocessing purpose.
+"""
 df = pd.read_csv('data/dataset01.csv')
 text = df['text'][0:1000]
 
@@ -20,12 +23,28 @@ print(vectr.get_feature_names_out())
 
 print(word_vectors.toarray())
 
+"""
+in above lines.
+get_feature_names_only() function will return the names of features. in this cases, every single word.
+then in next line, the toarray() function will convert the word vectors into array, so that we are
+easily able to see the vectors we have created for out text.
+"""
+
+
 #   2.  Manually using defaultdict
 
+#   creating a list of words
 text = text[0].split()
+
 bagofwords = defaultdict(int)
 
 for w in text:
     bagofwords[w] += 1
+
+"""
+the above loop will iterate over every word in the text.
+each words will have its own index position in bagofwords defaultdict represented by the word itself.
+And every occurance of the word will be counted in loop.
+"""
 
 print(bagofwords)
--- 001_preprocessing/008_ngrams.py
+++ 001_preprocessing/008_ngrams.py
@@ -1,11 +1,24 @@
+"""
+Somewhat similar to the previous bag of words.
+However, a key difference. The ngrams consider words not word (singular) for their counts.
+It assumes that words appearing together have more significance.
+It can be unigram, bigram, trigram or more. 
+in below example, we try the bigram i.e., two words.
+"""
+
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
 df = pd.read_csv('data/dataset01.csv')
 text = df['text'][100:150]
 
+"""
+in object creation, we pass the ngram value below using the ngram_range
+the first parameter to this is the minimum word limit and second parameter is the maximum word limit.
+"""
 vectr = CountVectorizer(ngram_range=(2,2))
 
 word_vectors = vectr.fit_transform(text)
 
 print(vectr.get_feature_names_out())
+
