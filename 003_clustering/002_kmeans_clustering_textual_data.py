cf20c98cb5f99727211c457a56d19ca9f17c15ca
mhassantrq <mhassantrq@gmail.com>
2026-05-09 13:43:39 +0500

    kmeans text added explanation

--- 003_clustering/002_kmeans_clustering_textual_data.py
+++ 003_clustering/002_kmeans_clustering_textual_data.py
@@ -7,10 +7,19 @@ df = pd.read_csv('data/dataset01.csv')
 
 # print(df.columns)
 # print(df.shape)
-
+"""
+Since we are not dealing with numbers but text. We first need to convert the text into numbers
+CountVectorizer with count every word the number of times its appearing.
+More explanation of CountVectorizer is in the Preprocessing directory in files of bagofwords and ngrams.
+"""
 vectorizer = CountVectorizer(stop_words='english', max_features=1000)
 text_vectors = vectorizer.fit_transform(df['text'])
 
+"""
+The first step is to create an object of the kmeans clustering model.
+We pass cluster value 2 in this case.
+Any value can be passed depending upon the number of clusters needed.
+"""
 kmeans_model = KMeans(n_clusters=2)
 
 df['tag'] = kmeans_model.fit_predict(text_vectors)
