074ffe571c8445b5b78a58b71b3d17861c50d2ac
mhassantrq <mhassantrq@gmail.com>
2026-05-14 18:44:38 +0500

    lectures start testing file

---
+++ 008_from zero/000_lectures/000_nlp.py
@@ -0,0 +1,13 @@
+"""
+01. Introduction to NLP
+02. Applications of NLP
+03. Text Preprocessing
+04. POS and NER
+05. Parsing
+06. Semantic Analysis
+07. ML for NLP
+08. DL for NLP
+09. Word Embeddings
+10. Text Vectorization
+11. Models
+"""
