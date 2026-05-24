0f7d0e01d45a29dffde442540bd847bd490e47b3
mhassantrq <mhassantrq@gmail.com>
2026-05-24 11:07:21 +0500

    updated pandas in libraries

--- 000_libraries and modules/001_pandas.py
+++ 000_libraries and modules/001_pandas.py
@@ -10,7 +10,7 @@ lets explore some uses of pandas below:
 #   import pandas library with alias 'pd', you can use different alias, but 'pd' just sound right. 
 import pandas as pd
 
-#   1. Reading data
+#       1. Reading data
 
 df = pd.read_csv('data/data.csv')   #   reading csv file. with parameter being the file path and name with extension
 
@@ -26,7 +26,7 @@ pandas can be used to read many types of files such as:
 """
 
 
-#   2. Provides data structures Series and Data Frames
+#       2. Provides data structures Series and Data Frames
 
 """
 Series is 1d. You can say a column.
@@ -45,11 +45,41 @@ data frame:
         akram   65      52      85
 """
 
-#   3. Display records
+#       3. Display records
 
 print(df.head())    #   head function displays first 5 rows
 
 print(df.tail())    #   tail function displays last 5 rows
 
 print(df['city'])   #   using square brackets, and column name, displays all values in the column
+
+#       4. Exploring Data
+
+print(df.info())        # summary information
+
+print(df.describe())    # statistical summary
+
+print(df.shape)         # display total rows and columns
+
+print(df.columns)       # display column names
+
+#       5. Sorting and aggregating data
+
+print(df['area'].sum())         # take sum of 'area' column
+
+print(df['area'].mean())         # take mean of 'area' column
+
+print(df['area'].min())         # take minimum of 'area' column
+
+print(df['area'].max())         # take maximum of 'area' column
+
+print(df.sort_values('area'))         # sort dataframe on 'area' column in ascending order
+
+#       6. handling missing values
+
+print(df.isna())                # display all rows and columns with boolean 'true' indicating if there is any missing value at exact position
+
+print(df.isna().sum())          # showing exactly how many missing values in each column of data frame
+
+print(df['area'].fillna(df['area'].mean()))     # fill every missing value in column 'area' with mean of that respective column
 
