fa17817ba614c6262f8da62e6f3ed3cae0a4917e
mhassantrq <mhassantrq@gmail.com>
2026-05-30 17:38:01 +0500

    added extra tree in decision tree classifier

--- 007_classification/000_decision_tree_sklearn.py
+++ 007_classification/000_decision_tree_sklearn.py
@@ -1,20 +1,27 @@
 """
 Decision Tree using sklearn
+1. Decision Tree
+2. Extra Tree
 """
 
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.tree import DecisionTreeClassifier, plot_tree, ExtraTreeClassifier
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('data/classification01.csv')
 
-#print(df)
-
+#   1. Decision Tree
 dt_model = DecisionTreeClassifier()
-
 dt_model.fit(df[['study', 'attendance', 'assignments']], df.result)
 
 print(dt_model.predict([[1,70,2]]))
+
+
+#   2. Extra Tree
+et_model = ExtraTreeClassifier()
+et_model.fit(df[['study', 'attendance', 'assignments']], df.result)
+
+print(et_model.predict([[5,85,6]]))
 
 
 """
