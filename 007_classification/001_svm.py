addd9574181b1b2920f11fa5f8a3ea829c6662cd
mhassantrq <mhassantrq@gmail.com>
2026-05-30 17:33:20 +0500

    updated svm

--- 007_classification/001_svm.py
+++ 007_classification/001_svm.py
@@ -4,6 +4,13 @@ SVM can be used for both classification and regression.
 """
 
 import pandas as pd
+from sklearn.svm import SVC
+
 
 df = pd.read_csv('data/classification01.csv')
 
+svc_model = SVC()
+svc_model.fit(df[['study']], df.result)
+
+pred_result = svc_model.predict([[3]])
+print(pred_result)
