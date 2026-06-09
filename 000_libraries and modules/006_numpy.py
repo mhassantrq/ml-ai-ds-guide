de92f6c1478c23f4c93ad41ec0ce19f788b7df1f
mhassantrq <mhassantrq@gmail.com>
2026-06-09 20:22:03 +0500

    updated numpy

--- 000_libraries and modules/006_numpy.py
+++ 000_libraries and modules/006_numpy.py
@@ -29,3 +29,29 @@ print(f'Dimension of array: {arr.ndim}')
 
 #   output shape of array
 print(f'Shape of array: {arr.shape}')
+
+#   output size of array
+print(f'Size of array: {arr.size}')
+
+#   output data type of array
+print(f'Data type of array: {arr.dtype}')
+
+#   array slice
+print(f'array: {arr[1:2, 0:1]}')    #   row from 1 to 2 and columns from 0 to 1
+print(f'array: {arr[:, 0:1]}')    #   all rows and columns from 0 to 1
+print(f'array: {arr[1:2, :]}')    #   row from 1 to 2 and all columns
+print(f'array: {arr[:, :]}')    #   all rows and all columns
+
+arr = np.array([
+    [1, 2, 3, 4],
+    [2, 4, 6, 8],
+    [3, 5, 7, 9]
+])
+
+print(f'{arr}')
+
+#   reshape array
+print(f'reshaped array: {arr.reshape(6,2)}')
+
+#   flatten array
+print(f'reshaped array: {arr.flatten()}')   #   flatten to one dimensional array
