9e45c3e670d9540746acb5838b59a2fc8be3d2fb
mhassantrq <mhassantrq@gmail.com>
2026-06-25 16:22:16 +0500

    testing lda

--- 004_topic modelling/001_lda_using_sklearn.py
+++ 004_topic modelling/001_lda_using_sklearn.py
@@ -4,29 +4,32 @@ from sklearn.decomposition import LatentDirichletAllocation
 from collections import defaultdict
 import re
 
-df = pd.read_csv('data/dataset.csv')
+df = pd.read_csv('data/dataset01.csv')
 
-temp_dataset = df['text'][0:5]
+temp_dataset = df['text'][11:12]
 dataset = []
 
+"""
+removing html tags and normalize words to lowercase
+"""
 for d in temp_dataset:
     d = re.sub(r'<.*?>', '', d)
     d = d.lower()
     dataset.append(d)
 
-#vect = CountVectorizer(stop_words='english')
-vect = TfidfVectorizer(stop_words='english')
+vect = CountVectorizer(stop_words='english')
+#vect = TfidfVectorizer(stop_words='english')
 words_count = vect.fit_transform(dataset)
 
 words = vect.get_feature_names_out()
 
-lda = LatentDirichletAllocation(n_components=5)
+lda = LatentDirichletAllocation(n_components=3)
 lda.fit(words_count)
 
-t_list = []
+t_list=''
 
 for t in lda.components_:
     top = t.argmax()
-    t_list.append(words[top])
+    t_list += str(words[top]) + ' '
 
 print(t_list)
