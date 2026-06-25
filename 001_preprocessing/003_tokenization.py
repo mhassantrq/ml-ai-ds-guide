170a1729e4cbe785c54e741c333ab97e75457954
mhassantrq <mhassantrq@gmail.com>
2026-06-25 13:49:14 +0500

    updated tokenization in preprocessing

--- 001_preprocessing/003_tokenization.py
+++ 001_preprocessing/003_tokenization.py
@@ -6,7 +6,10 @@ or dividing the given data into small parts, components, words, sentences, whate
 var_text = 'hello, this is python coding'
 
 
-#   one way to split the above text into smaller parts is to use split() function
+#   1. Using Split
+
+
+#   the simplest and naive way to split the above text into smaller parts is to use split() function
 #   this returns a list of words separated by empty spaces
 token_text = var_text.split()
 
@@ -16,7 +19,7 @@ print(token_text)   #   lets see the list of words separated
 the above method is straight forward but not fully effective.
 """
 
-
+#   2. using regex
 
 """
 lets try tokenizing using regex library
@@ -31,3 +34,15 @@ var_text = 'hello, this is python coding!'
 token_text = re.findall(r'\w+', var_text)
 
 print(token_text)
+
+
+"""
+using regex, you may notice that the previous method of using split() included any attached puntutation with the words.
+"""
+
+#   3. character tokenization
+
+var_text = 'hello, this is python coding!'
+
+char_tokens = list(var_text)
+print(f'character tokens: {char_tokens}')
