2f5fe6b107bb02c8d08cb7eb3043d3dbf9643bcc
mhassantrq <mhassantrq@gmail.com>
2026-08-03 14:40:16 +0500

    updated

--- 000_libraries and modules/007_nltk.py
+++ 000_libraries and modules/007_nltk.py
@@ -10,13 +10,23 @@ text = df['text'][0]
 
 
 """
-tokenization using nltk has two options, 
-tokenize words or tokenize sentences.
+1. tokenization
+tokenization using nltk has three options, 
+tokenize words, tokenize sentences or tokenize using regular expressions.
+
 for more on the topic tokenization itself, refer to tokenization file in preprocessing folder.
+for more no learning regular expressions, refer to regular expressions file in preprocessing folder.
 """
 
 word_tokens = nltk.word_tokenize(text)
 sent_tokens = nltk.sent_tokenize(text)
+regex_tokens = nltk.regexp_tokenize(text, pattern='\w+')
 
 print(f'word tokens: {word_tokens}')
 print(f'sentence tokens: {sent_tokens}')
+print(f'regular expression tokens: {regex_tokens}')
+
+
+"""
+2. 
+"""
